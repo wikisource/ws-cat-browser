5221c3c7690bf64ed556f2acedbb3204defb996d
Sam Wilson <sam@samwilson.id.au>
2015-02-08 12:15:34 +0800

    Separate data out of HTML.

--- build.php
+++ build.php
@@ -6,15 +6,12 @@ if (!isset($dsn) || !isset($dbuser) || !isset($dbpass)) {
     echo 'config.php must define $dsn, $dbuser, and $dbpass' . "\n";
     exit(1);
 }
-
-//getMostRecentDump();exit();
-
 $pdo = new PDO($dsn, $dbuser, $dbpass);
 
 
 echo "Getting list of validated works . . . ";
 $validatedWorks = getValidatedWorks($pdo);
-echo "done.\n";
+echo "done\n";
 
 
 echo "Getting category data . . . ";
@@ -23,31 +20,21 @@ foreach ($validatedWorks as $indexTitle => $workTitle) {
     $catTree = getAllCats($pdo, $workTitle, 0);
     $allCats = array_map('array_unique', array_merge_recursive($allCats, $catTree));
 }
-echo "done.\n";
+echo "done\n";
 
 
 echo "Sorting categories . . . ";
 foreach ($allCats as $cat => $cats) {
     sort($allCats[$cat]);
 }
-echo "done.\n";
+echo "done\n";
 
 
-// Compile templates etc.
-echo "Compiling HTML list . . . ";
-$outDir = __DIR__ . '/out';
-$templateDir = __DIR__ . '/templates';
-recurse_copy(__DIR__ . '/templates', $outDir);
-$loader = new Twig_Loader_Filesystem($templateDir);
-$twig = new Twig_Environment($loader);
-$included = array();
-$list = printCatTree($allCats, $allCats['Category:Categories'], 0, $validatedWorks, $included);
-$header = $twig->render('header.html', array('total' => number_format(count($included))));
-$footer = $twig->render('footer.html', array('now' => date('j F, Y'),));
-file_put_contents($outDir . '/index.html', $header . $list . $footer);
-echo "done.\n";
-
+echo "Writing categories.json\n";
+file_put_contents('categories.json', json_encode($allCats));
+exit(0);
 // End
+
 
 /**
  * Functions only beyond here.
@@ -159,75 +146,4 @@ function getCats($pdo, $baseCat, $ns) {
         $cats[] = 'Category:' . $cat['catname'];
     }
     return $cats;
-}
-
-function printCatTree($allcats, $cats, $depth, $validatedWorks, &$included) {
-    $list = "<ol class='c'>\n";
-    //print_r($cats);
-    foreach ($cats as $cat) {
-        //echo "Writing $cat to outfile.\n";
-        //print_r($allcats[$cat]);
-        //$ref = cleanName($cat);
-        $title = str_replace('_', ' ', $cat);
-        if (in_array($cat, $validatedWorks)) {
-            //echo "is a work.\n";
-            $title = "<a href='https://en.wikisource.org/wiki/$cat' title='View on Wikisource'>$title</a>"
-                    . "<a href='http://wsexport.wmflabs.org/tool/book.php?lang=en&format=epub&page=$cat'>"
-                    . " <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/EPUB_silk_icon.svg/15px-EPUB_silk_icon.svg.png'"
-                    . "     title='Download EPUB' />"
-                    . "</a>";
-            $included[$cat] = $cat;
-        }
-        $list .= "<li><span>" . str_replace('Category:', '', $title) . "</span>\n";
-        if (isset($allcats[$cat])) {
-            $list .= printCatTree($allcats, $allcats[$cat], $depth + 1, $validatedWorks, $included);
-        }
-        $list .= "</li>\n";
-    }
-    $list .= "</ol>\n";
-    return $list;
-}
-
-//function cleanName($name) {
-//    return preg_filter('/[^a-zA-Z0-9_-]/', '', $name);
-//}
-
-/**
- * 
- * @link http://stackoverflow.com/a/2050909 Author of this function.
- * @param type $src
- * @param type $dst
- */
-function recurse_copy($src, $dst) {
-    $dir = opendir($src);
-    @mkdir($dst);
-    while (false !== ( $file = readdir($dir))) {
-        if (( $file != '.' ) && ( $file != '..' )) {
-            if (is_dir($src . '/' . $file)) {
-                recurse_copy($src . '/' . $file, $dst . '/' . $file);
-            } else {
-                copy($src . '/' . $file, $dst . '/' . $file);
-            }
-        }
-    }
-    closedir($dir);
-}
-
-function getMostRecentDump() {
-    $abstractUrl = 'https://dumps.wikimedia.org/enwikisource/latest/enwikisource-latest-abstract.xml-rss.xml';
-    $feed = file_get_contents($abstractUrl);
-    if ($feed === FALSE) {
-        return FALSE;
-    }
-    $xml = simplexml_load_string($feed);
-    $pubDate = $xml->channel->item->pubDate;
-    $dateStr = date('Ymd', strtotime($pubDate));
-
-    // Download table dumps.
-    $baseUrl = 'https://dumps.wikimedia.org/enwikisource/';
-    $tables = array('page', 'categorylinks', 'pagelinks');
-    foreach ($tables as $table) {
-        $url = "$baseUrl$dateStr/enwikisource-$dateStr-$table.sql.gz";
-        echo "$url\n";
-    }
 }
