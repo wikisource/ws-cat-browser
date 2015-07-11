29d57bbb95afb4e72a5282511d3646c78fe2ccf1
Sam Wilson <sam@samwilson.id.au>
2015-07-11 08:24:09 +0800

    Fix security. Also links.

--- index.php
+++ index.php
@@ -4,8 +4,9 @@ require 'config.php';
 $lang = (isset($_GET['lang'])) ? htmlspecialchars($_GET['lang']) : 'en';
 if ( !array_key_exists($lang, $dbs)) {
     $err = "The language '$lang' has not yet been included. Please lodge an issue.";
+    $lang = 'en';
 }
-$suffix = $lang=='en' ? '' : '_'.$lang;
+$suffix = ($lang=='en') ? '' : '_'.$lang;
 
 ?><!doctype html>
 <html class="no-js" lang="en">
@@ -55,8 +56,8 @@ $suffix = $lang=='en' ? '' : '_'.$lang;
                 </p>
                 <p>
                     For more information please see
-                    <a href="https://github.com/samwilson/wikisource-cat-browser">the code</a> on Github
-                    or contact <a href="https://en.wikisource.org/wiki/User:Samwilson">User:Samwilson</a>.
+                    <a href="https://github.com/wikisource/ws-cat-browser">the code</a> on Github
+                    or contact <a href="https://meta.wikimedia.org/wiki/User:Samwilson">User:Samwilson</a>.
                     The above data is available in
                     <a href="works<?php echo $suffix ?>.json"><tt>works<?php echo $suffix ?>.json</tt></a>
                     and <a href="categories<?php echo $suffix ?>.json"><tt>categories<?php echo $suffix ?>.json</tt></a>.
