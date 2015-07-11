f3593a9e4d59fe85fc12da241203b1f5dffc4877
Ricordisamoa <ricordisamoa@openmailbox.org>
2015-07-11 11:19:26 +0200

    Get index roots from Wikidata

--- config_dist.php
+++ config_dist.php
@@ -4,22 +4,24 @@ $dbuser = '';
 
 $dbpass = '';
 
+$wbapi = 'https://www.wikidata.org/w/api.php';
+
+$wbitem = 'Q15634466';
+
 $dbs = array(
 
     'en' => array(
-        'pdo'        => 'mysql:dbname=enwikisource_p;host=enwikisource.labsdb',
+        'db_name'    => 'enwikisource',
         'cat_label'  => 'Category',
         'cat_root'   => 'Categories',
         'index_ns'   => 106,
-        'index_root' => 'Index_Validated',
     ),
 
     'it' => array(
-        'pdo'        => 'mysql:dbname=itwikisource_p;host=itwikisource.labsdb',
+        'db_name'    => 'itwikisource',
         'cat_label'  => 'Categoria',
         'cat_root'   => 'Categorie',
         'index_ns'   => 110,
-        'index_root' => 'Pagine_indice_SAL_100%',
     ),
 
 );
